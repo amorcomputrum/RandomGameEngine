d6bc009217b87da98340f4e0bdcb35a99b20be57
AmorComputrum <43394067+amorcomputrum@users.noreply.github.com>
2022-07-26 12:07:48 -0500

    Delete src/CMakeFiles directory

--- src/CMakeFiles/RGE.dir/Physics/Box.cpp.o.d
+++
@@ -1,6 +0,0 @@
-src/CMakeFiles/RGE.dir/Physics/Box.cpp.o: \
- /run/media/benajah/VMDRIVER/RGE/src/Physics/Box.cpp \
- /usr/include/stdc-predef.h \
- /run/media/benajah/VMDRIVER/RGE/src/../include/RGE/Physics/Box.h \
- /run/media/benajah/VMDRIVER/RGE/src/../include/RGE/Physics/Shape.h \
- /run/media/benajah/VMDRIVER/RGE/src/../include/RGE/System/Vector2d.h
--- src/CMakeFiles/RGE.dir/Window/Camera.cpp.o.d
+++
@@ -1,4 +0,0 @@
-src/CMakeFiles/RGE.dir/Window/Camera.cpp.o: \
- /run/media/benajah/VMDRIVER/RGE/src/Window/Camera.cpp \
- /usr/include/stdc-predef.h \
- /run/media/benajah/VMDRIVER/RGE/src/../include/RGE/Window/Camera.h
